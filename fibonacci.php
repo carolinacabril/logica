e3a2ca849580bcad7b294bd3038e400aeb72a0a5
php 13 <php.13@tom.cec.ime.usp.br>
2019-01-29 22:44:27 -0200

    programa fibonacci

---
+++ fibonacci.php
@@ -0,0 +1,18 @@
+<?php 
+
+function fibonacci($n) {
+	if($n < 2){
+	return $n;
+} else {
+
+	return fibonacci($n-1) + fibonacci($n - 2);
+}
+
+}
+
+for($i=1; $i <= 10; $i++) {
+
+	echo fibonacci($i)." ".PHP_EOL;
+}
+
+?>
